5fdfa860acb65a3e98c125b9928432605aaa5a36
dawnos <litang@gmail.com>
2020-05-14 00:49:18 +0200

    Update pub list

--- scripts/bib2yml.py
+++ scripts/bib2yml.py
@@ -11,13 +11,27 @@ style = pybtex.plugin.find_plugin('pybtex.style.formatting', 'plain')()
 backend = pybtex.plugin.find_plugin('pybtex.backends', 'html')()
 
 
-def to_yaml(filename):
+def get_priority(entry):
+    first_name_of_first_author = entry.persons['author'][0].first_names
+    last_name_of_first_author = entry.persons['author'][0].last_names
+    if len(first_name_of_first_author) == 1 and first_name_of_first_author[0] == 'Li' and \
+       len(last_name_of_first_author) == 1 and last_name_of_first_author[0] == 'Tang':
+        return 0
+    else:
+        return 1
+
+
+def to_yaml(filename, n_select=3):
 
     items = []
-
     bibs = pybtex.database.parse_file(filename)
+    priorities = [get_priority(bibs.entries[k]) for k in bibs.entries]
+    sorted_priorities_index = sorted(range(len(priorities)),key=priorities.__getitem__)
+    is_selected = [False, ] * len(priorities)
+    for i in range(min(n_select, len(priorities))):
+        is_selected[sorted_priorities_index[i]] = True
 
-    for citekey in bibs.entries:
+    for idx, citekey in enumerate(bibs.entries):
         item = {}
         entry = bibs.entries[citekey]
         item['ref'] = style.format_entry(0, entry).text.render(backend)
@@ -25,10 +39,12 @@ def to_yaml(filename):
         item['code'] = ''
         item['pdf'] = '%s.pdf' % citekey
         item['bib'] = entry2bibtex(citekey, entry)
-
-        #
-        # item['ref'] = item['ref'].replace("Li&nbsp;Tang", "<b>Li&nbsp;Tang</b>")
-        # item['ref'] = item['ref'].replace("Li Tang", "<b>Li Tang</b>")
+        item['selected'] = True if is_selected[idx] else False
+        # if 'priority' in entry.fields:
+        #     item['priority'] = entry.fields['priority']
+        # else:
+        #     item['priority'] = 99
+        # item['index'] = idx
 
         items.append(item)
 
